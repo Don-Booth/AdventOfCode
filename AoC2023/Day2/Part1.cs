e3d56bf28b104209a3ce5080874c627b3604b268
Don Booth <Don.Booth@live.com>
2023-12-08 21:37:25 -0400

    moved code into part1 class

---
+++ AoC2023/Day2/Part1.cs
@@ -0,0 +1,80 @@
+﻿using Day2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    internal class Part1
+    {
+        public static int GetSumOfPossibleGameIDs(List<Game> games)
+        {
+            int result = 0;
+
+            foreach (Game game in games)
+            {
+                if (game.Possibility)
+                {
+                    result += game.GameID;
+                }
+            }
+
+            return result;
+        }
+
+        public static List<Game> LoadGamesIntoList(string fileName, BagMaximums bagMaximums)
+        {
+            List<Game> result = new();
+
+            foreach (string line in File.ReadLines(fileName))
+            {
+                Game game = new Game();
+
+                game.GameID = int.Parse(line.Substring(line.IndexOf(' ', 0), line.IndexOf(':') - line.IndexOf(' ', 0)));
+
+                string linesubsets = line.Substring(line.IndexOf(":") + 2);
+
+                foreach (string subset in linesubsets.Split(';'))
+                {
+                    Subset subsetToLoadToGame = new();
+
+                    foreach (string set in subset.Trim().Split(","))
+                    {
+                        var setArray = set.Trim().Split(" ");
+                        int count = int.Parse((string)setArray[0]);
+
+                        switch (setArray[1])
+                        {
+                            case "red":
+                                subsetToLoadToGame.RedCount = count;
+                                break;
+                            case "green":
+                                subsetToLoadToGame.GreenCount = count;
+                                break;
+                            case "blue":
+                                subsetToLoadToGame.BlueCount = count;
+                                break;
+                        }
+                    }
+
+                    game.Subsets.Add(subsetToLoadToGame);
+                }
+
+                foreach (Subset subsetBagMaxCheck in game.Subsets)
+                {
+                    if (subsetBagMaxCheck.RedCount > bagMaximums.MaxRed || subsetBagMaxCheck.GreenCount > bagMaximums.MaxGreen || subsetBagMaxCheck.BlueCount > bagMaximums.MaxBlue)
+                    {
+                        game.Possibility = false;
+                        break;
+                    }
+                }
+
+                result.Add(game);
+            }
+
+            return result;
+        }
+    }
+}
--- AoC2023/Day2/Program.cs
+++ AoC2023/Day2/Program.cs
@@ -41,79 +41,10 @@ internal class Program
         int total = 0;
         BagMaximums bagMaximums = new BagMaximums(12, 13, 14);
 
-        games = LoadGamesIntoList("input.txt", bagMaximums);
-        total = GetSumOfPossibleGameIDs(games);
+        games = Part1.LoadGamesIntoList("input.txt", bagMaximums);
+        total = Part1.GetSumOfPossibleGameIDs(games);
 
-        Console.WriteLine(total);
+        Console.WriteLine(total); // The correct answer is 2105
 
-        // The correct answer is 2105
-    }
-
-    private static int GetSumOfPossibleGameIDs(List<Game> games)
-    {
-        int result = 0;
-
-        foreach (Game game in games)
-        {
-            if (game.Possibility)
-            {
-                result += game.GameID;
-            }
-        }
-
-        return result;
-    }
-
-    private static List<Game> LoadGamesIntoList(string fileName, BagMaximums bagMaximums)
-    {
-        List<Game> result = new();
-
-        foreach (string line in File.ReadLines(fileName))
-        {
-            Game game = new Game();
-
-            game.GameID = int.Parse(line.Substring(line.IndexOf(' ', 0), line.IndexOf(':') - line.IndexOf(' ', 0)));
-
-            string linesubsets = line.Substring(line.IndexOf(":") + 2);
-
-            foreach(string subset in linesubsets.Split(';'))
-            {
-                Subset subsetToLoadToGame = new();
-
-                foreach (string set in subset.Trim().Split(","))
-                {
-                    var setArray = set.Trim().Split(" ");
-                    int count = int.Parse((string)setArray[0]);
-
-                    switch (setArray[1])
-                    {
-                        case "red":
-                            subsetToLoadToGame.RedCount = count;
-                            break;
-                        case "green":
-                            subsetToLoadToGame.GreenCount = count;
-                            break;
-                        case "blue":
-                            subsetToLoadToGame.BlueCount = count;
-                            break;
-                    }
-                }
-
-                game.Subsets.Add(subsetToLoadToGame);
-            }
-
-            foreach (Subset subsetBagMaxCheck in game.Subsets)
-            {
-                if (subsetBagMaxCheck.RedCount > bagMaximums.MaxRed || subsetBagMaxCheck.GreenCount > bagMaximums.MaxGreen || subsetBagMaxCheck.BlueCount > bagMaximums.MaxBlue)
-                {
-                    game.Possibility = false;
-                    break;
-                }
-            }
-
-            result.Add(game);
-        }
-
-        return result;
     }
 }
